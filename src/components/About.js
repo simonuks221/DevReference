98ac896ee1f789ad0d28b0c90b6377f562c6befa
simonuks221 <simonasriauka@gmail.com>
2021-07-03 20:08:21 +0300

    REmoved animations scrollbars

--- src/components/About.js
+++ src/components/About.js
@@ -28,7 +28,7 @@ const About = () => {
     }, [])
 
     return (
-            <Container fluid className = 'bg-secondary rounded py-2 text-center px-5 text-light'>
+            <Container fluid className = 'bg-secondary rounded py-2 text-center px-5 text-light my-2'>
                 <h2 className = 'text-center text-light py-2'>About</h2>
                 <p className = ''>My created webpage to quickly see the reference code and resources I need weather would it be Web development or doing custom charts</p>
                 <p className = ''>Created using ReactJS and stylized using Bootstrap css library and components</p>
